a27ec64449fdceeea81ca5b07bf193a3a8de06bd
Aman Prajapati <ap757162@gmail.com>
2025-03-20 10:31:55 +0530

    [Aman] Add. Set up the Eureka Server.

---
+++ Eureka_Server/Eureka_Server/src/main/java/com/example/Eureka_Server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Eureka_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer  // Enables Eureka Server
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
